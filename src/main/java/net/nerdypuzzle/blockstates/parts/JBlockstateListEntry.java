ee9efc13d88cf34594707139099f2cef765e43ce
NerdyPuzzle <112079900+NerdyPuzzle@users.noreply.github.com>
2024-10-31 09:44:29 +0200

    Update to 1.5.1
    Fix #5

--- src/main/java/net/nerdypuzzle/blockstates/parts/JBlockstateListEntry.java
+++ src/main/java/net/nerdypuzzle/blockstates/parts/JBlockstateListEntry.java
@@ -103,7 +103,7 @@ public class JBlockstateListEntry extends JSimpleListEntry<Blockstates.Blockstat
         destal.add(ComponentUtils.squareAndBorder(this.textureFront, L10N.t("elementgui.block.texture_place_front_side", new Object[0])));
         destal.add(ComponentUtils.squareAndBorder(this.textureRight, L10N.t("elementgui.block.texture_place_right", new Object[0])));
         destal.add(ComponentUtils.squareAndBorder(this.textureBack, L10N.t("elementgui.block.texture_place_back", new Object[0])));
-        this.textureLeft.setActionListener((event) -> {
+        this.textureLeft.addTextureSelectedListener((event) -> {
             if (!this.texture.hasTexture() && !this.textureTop.hasTexture() && !this.textureBack.hasTexture() && !this.textureFront.hasTexture() && !this.textureRight.hasTexture()) {
                 this.texture.setTexture(this.textureLeft.getTextureHolder());
                 this.textureTop.setTexture(this.textureLeft.getTextureHolder());
